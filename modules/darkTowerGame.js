1b23bbbd87b764b0e9109464d7f71de1b3c10c37
Ian Marshall <ian.js.tutor@gmail.com>
2024-03-08 16:36:10 -0500

    inventory

--- modules/darkTowerGame.js
+++ modules/darkTowerGame.js
@@ -25,7 +25,7 @@ class DarkTowerGame {
                 ["silverKey", false],
                 ["goldKey", false]
             ]),
-            move: new Map([
+            chance_move: new Map([
                 ["safe", 1],
                 ["battle", 2],
                 ["dragon", 0],
@@ -33,12 +33,12 @@ class DarkTowerGame {
                 ["cursed", 0],
                 ["lost", 0.5]
             ]),
-            tomb: new Map([
+            chance_tomb: new Map([
                 ["empty", 0.5],
                 ["battle", 2],
                 ["treasure", 0]
             ]),
-            treasure: new Map([
+            chance_treasure: new Map([
                 ["gold", 1],
                 ["key", 0],
                 ["sword", 0],
@@ -65,6 +65,7 @@ class DarkTowerGame {
 class DarkTowerStates {
     static start(player) {
         return {
+            name: "start",
             keys: "000010111111",
             output: null,
             img: null,
@@ -77,6 +78,304 @@ class DarkTowerStates {
                 frontier: "frontier",
                 inventory: "inventory_warriors"
             }
+        };
+    }
+
+    static bazaar(player) {
+        return {
+            name: "bazaar"
+        };
+    }
+
+    static tomb(player) {
+        return {
+            name: "tomb"
+        };
+    }
+
+    static move(player) {
+        return {
+            name: "move"
+        };
+    }
+
+    static sanctuary(player) {
+        return {
+            name: "sanctuary"
+        };
+    }
+
+    static darkTower(player) {
+        return {
+            name: "darkTower"
+        };
+    }
+
+    static frontier(player) {
+        return {
+            name: "frontier"
+        };
+    }
+
+    static inventory_warriors(player, dt) {
+        return {
+            name: "inventory_warriors",
+            keys: "100001000000",
+            output: player.inventory.get("warriors"),
+            img: dt.media.image.warriors,
+            state: {
+                yes: "inventory_gold",
+                clear: "start"
+            }
+        };
+    }
+
+    static inventory_gold(player, dt) {
+        return {
+            name: "inventory_gold",
+            keys: "110001000000",
+            output: player.inventory.get("gold"),
+            img: dt.media.image.gold,
+            state: {
+                yes: "inventory_food",
+                repeat: "inventory_warriors",
+                clear: "start"
+            }
+        };
+    }
+
+    static inventory_food(player, dt) {
+        let keys = "110001000000";
+        let state = {
+            repeat: "inventory_warriors",
+            clear: "start"
+        };
+        let yes = [
+            "beast",
+            "scout",
+            "healer",
+            "sword",
+            "wizard",
+            "brassKey",
+            "silverKey",
+            "goldKey"
+        ].find(item => Boolean(player.inventory.get(item)));
+        if (yes) {
+            state.yes = `inventory_${yes}`;
+        }
+        else {
+            keys = "010001000000";
+        }
+        return {
+            name: "inventory_food",
+            keys,
+            output: player.inventory.get("food"),
+            img: dt.media.image.food,
+            state
+        };
+    }
+
+    static inventory_beast(player, dt) {
+        let keys = "110001000000";
+        let state = {
+            repeat: "inventory_warriors",
+            clear: "start"
+        };
+        let yes = [
+            "scout",
+            "healer",
+            "sword",
+            "wizard",
+            "brassKey",
+            "silverKey",
+            "goldKey"
+        ].find(item => Boolean(player.inventory.get(item)));
+        if (yes) {
+            state.yes = `inventory_${yes}`;
+        }
+        else {
+            keys = "010001000000";
+        }
+        return {
+            name: "inventory_beast",
+            keys,
+            output: "01",
+            img: dt.media.image.beast,
+            state
+        };
+    }
+
+    static inventory_scout(player, dt) {
+        let keys = "110001000000";
+        let state = {
+            repeat: "inventory_warriors",
+            clear: "start"
+        };
+        let yes = [
+            "healer",
+            "sword",
+            "wizard",
+            "brassKey",
+            "silverKey",
+            "goldKey"
+        ].find(item => Boolean(player.inventory.get(item)));
+        if (yes) {
+            state.yes = `inventory_${yes}`;
+        }
+        else {
+            keys = "010001000000";
+        }
+        return {
+            name: "inventory_scout",
+            keys,
+            output: "01",
+            img: dt.media.image.scout,
+            state
+        };
+    }
+
+    static inventory_healer(player, dt) {
+        let keys = "110001000000";
+        let state = {
+            repeat: "inventory_warriors",
+            clear: "start"
+        };
+        let yes = [
+            "sword",
+            "wizard",
+            "brassKey",
+            "silverKey",
+            "goldKey"
+        ].find(item => Boolean(player.inventory.get(item)));
+        if (yes) {
+            state.yes = `inventory_${yes}`;
+        }
+        else {
+            keys = "010001000000";
+        }
+        return {
+            name: "inventory_healer",
+            keys,
+            output: "01",
+            img: dt.media.image.healer,
+            state
+        };
+    }
+
+    static inventory_sword(player, dt) {
+        let keys = "110001000000";
+        let state = {
+            repeat: "inventory_warriors",
+            clear: "start"
+        };
+        let yes = [
+            "wizard",
+            "brassKey",
+            "silverKey",
+            "goldKey"
+        ].find(item => Boolean(player.inventory.get(item)));
+        if (yes) {
+            state.yes = `inventory_${yes}`;
+        }
+        else {
+            keys = "010001000000";
+        }
+        return {
+            name: "inventory_sword",
+            keys,
+            output: "01",
+            img: dt.media.image.sword,
+            state
+        };
+    }
+
+    static inventory_wizard(player, dt) {
+        let keys = "110001000000";
+        let state = {
+            repeat: "inventory_warriors",
+            clear: "start"
+        };
+        let yes = [
+            "brassKey",
+            "silverKey",
+            "goldKey"
+        ].find(item => Boolean(player.inventory.get(item)));
+        if (yes) {
+            state.yes = `inventory_${yes}`;
+        }
+        else {
+            keys = "010001000000";
+        }
+        return {
+            name: "inventory_wizard",
+            keys,
+            output: "01",
+            img: dt.media.image.wizard,
+            state
+        };
+    }
+
+    static inventory_brassKey(player, dt) {
+        let keys = "110001000000";
+        let state = {
+            repeat: "inventory_warriors",
+            clear: "start"
+        };
+        let yes = [
+            "silverKey",
+            "goldKey"
+        ].find(item => Boolean(player.inventory.get(item)));
+        if (yes) {
+            state.yes = `inventory_${yes}`;
+        }
+        else {
+            keys = "010001000000";
+        }
+        return {
+            name: "inventory_brassKey",
+            keys,
+            output: "01",
+            img: dt.media.image.brasskey,
+            state
+        };
+    }
+
+    static inventory_silverKey(player, dt) {
+        let keys = "110001000000";
+        let state = {
+            repeat: "inventory_warriors",
+            clear: "start"
+        };
+        let yes = [
+            "goldKey"
+        ].find(item => Boolean(player.inventory.get(item)));
+        if (yes) {
+            state.yes = `inventory_${yes}`;
+        }
+        else {
+            keys = "010001000000";
+        }
+        return {
+            name: "inventory_silverKey",
+            keys,
+            output: "01",
+            img: dt.media.image.silverkey,
+            state
+        };
+    }
+
+    static inventory_goldKey(player, dt) {
+        let keys = "010001000000";
+        let state = {
+            repeat: "inventory_warriors",
+            clear: "start"
+        };
+        return {
+            name: "inventory_silverKey",
+            keys,
+            output: "01",
+            img: dt.media.image.goldkey,
+            state
         };
     }
 }
